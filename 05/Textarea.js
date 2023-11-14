24e6af09735385ef2b08cff00e1c0bfe1b394d4f
mikroTesting <nowakowski.m1995@gmail.com>
2023-11-14 20:19:47 +0100

    task 05 done

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
 
 class Textarea extends React.Component {
-    render() {
-        return <textarea></textarea>
+  getSnapshotBeforeUpdate() {
+    const offsetHeight = this.textarea.offsetHeight;
+
+    return offsetHeight < 100 ? { resize: true } : { resize: false };
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (
+      this.textarea.offsetHeight < this.textarea.scrollHeight &&
+      snapshot.resize
+    ) {
+      this.textarea.style.height = this.textarea.offsetHeight + 11 + "px";
     }
+  }
+  render() {
+    const { content, handleChange } = this.props;
+    return (
+      <textarea
+        ref={(node) => (this.textarea = node)}
+        value={content}
+        onChange={handleChange}
+      ></textarea>
+    );
+  }
 }
 
 export default Textarea;
--- 05/app.js
+++ 05/app.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import Textarea from './Textarea';
+import Textarea from "./Textarea";
 
 class App extends React.Component {
-    state = {
-        text: '',
-    }
+  state = {
+    text: "",
+  };
 
-    render() {
-        const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
-    }
+  handleChange = (e) => {
+    const { value } = e.target;
+
+    this.setState({
+      text: value,
+    });
+  };
+
+  render() {
+    const { text } = this.state;
+    return <Textarea handleChange={this.handleChange} content={text} />;
+  }
 }
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 root.render(<App />);
